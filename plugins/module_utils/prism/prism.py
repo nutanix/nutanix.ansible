8771eadb5f5441556d4ffafa9235c0f026862107
Prem Karat <pkarat@mvista.com>
2021-12-02 14:56:53 +0530

    initial client sdk with inventory plugin includes 1. Client SDK 2. Inventory plugin 3. Collection skeleton code 4. examples for plugin
    Signed-off-by: Prem Karat <pkarat@mvista.com>

---
+++ plugins/module_utils/prism/prism.py
@@ -0,0 +1,12 @@
+from __future__ import absolute_import, division, print_function
+__metaclass__ = type
+
+from ..entity import Entity
+
+
+class Prism(Entity):
+    __BASEURL__ = "/api/nutanix/v3"
+
+    def __init__(self, module, resource_type):
+        resource_type = self.__BASEURL__ + resource_type
+        super().__init__(module, resource_type)
