aff6118965e2b5d96c47908f0c0196b2d2e400f4
Gevorg-Khachatryaan <Gevorg1050.1>
2022-01-28 18:30:14 +0400

    requirements for vm name

---
+++ plugins/module_utils/prism/prism.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+from ..entity import Entity
+
+
+class Prism(Entity):
+    __BASEURL__ = "/api/nutanix/v3"
+
+    def __init__(self, module, resource_type):
+        resource_type = self.__BASEURL__ + resource_type
+        super().__init__(module, resource_type)
