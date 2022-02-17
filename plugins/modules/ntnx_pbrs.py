3cdcc89c34d7a053694d0713e0d6db9e16217fbb
alaa-bish <alaa.bishtawi@nutanix.com>
2022-02-17 11:52:29 +0200

    Fix Isort

--- plugins/modules/ntnx_pbrs.py
+++ plugins/modules/ntnx_pbrs.py
@@ -405,8 +405,8 @@ task_uuid:
 """
 
 from ..module_utils.base_module import BaseModule  # noqa: E402
-from ..module_utils.prism.tasks import Task  # noqa: E402
 from ..module_utils.prism.pbrs import Pbr  # noqa: E402
+from ..module_utils.prism.tasks import Task  # noqa: E402
 from ..module_utils.utils import remove_param_with_none_value  # noqa: E402
 
 
