b29add742f052a874ba69fb36580c7c1529ec87b
Prem Karat <prem.karat@gmail.com>
2022-02-23 19:21:50 +0530

    Base module for foundation
    Signed-off-by: Prem Karat <prem.karat@gmail.com>

---
+++ plugins/module_utils/foundation/foundation.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+from ..entity import Entity
+
+
+class Foundation(Entity):
+    __BASEURL__ = "/foundation"
+
+    def __init__(self, module, resource_type):
+        resource_type = self.__BASEURL__ + resource_type
+        super(Foundation, self).__init__(module, resource_type, scheme="http")
