664dbeb4e100d25722bd1dad9a972dd9934585b3
alaa-bish <alaa.bishtawi@nutanix.com>
2022-03-20 10:05:04 +0200

    Flake8 fixes

--- plugins/module_utils/entity.py
+++ plugins/module_utils/entity.py
@@ -2,8 +2,6 @@
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import absolute_import, division, print_function
 
-from urllib import response
-
 __metaclass__ = type
 
 import json
