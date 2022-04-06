5f2799e2bea8c7785aabb22f7aeba879872b4257
Gevorg-Khachatryaan <Gevorg1050.1>
2022-04-06 16:55:09 +0400

    #166 fixes for arguments spec

--- plugins/modules/ntnx_vms_clone.py
+++ plugins/modules/ntnx_vms_clone.py
@@ -37,59 +37,6 @@ from ..module_utils.prism.vms import VM  # noqa: E402
 from ..module_utils import utils
 
 
-def get_module_spec():
-    mutually_exclusive = [("name", "uuid")]
-
-    entity_by_spec = dict(name=dict(type="str"), uuid=dict(type="str"))
-
-    network_spec = dict(
-        uuid=dict(type="str"),
-        state=dict(type="str", choices=["absent"]),
-        subnet=dict(
-            type="dict", options=entity_by_spec, mutually_exclusive=mutually_exclusive
-        ),
-        private_ip=dict(type="str", required=False),
-        is_connected=dict(type="bool", default=True),
-    )
-
-    boot_config_spec = dict(
-        boot_type=dict(
-            type="str", choices=["LEGACY", "UEFI", "SECURE_BOOT"], default="LEGACY"
-        ),
-        boot_order=dict(
-            type="list", elements="str", default=["CDROM", "DISK", "NETWORK"]
-        ),
-    )
-
-    gc_spec = dict(
-        type=dict(type="str", choices=["cloud_init", "sysprep"], required=True),
-        script_path=dict(type="path", required=True),
-        is_overridable=dict(type="bool", default=False),
-    )
-
-    module_args = dict(
-        name=dict(type="str", required=False),
-        vm_uuid=dict(type="str"),
-        desc=dict(type="str"),
-        project=dict(
-            type="dict", options=entity_by_spec, mutually_exclusive=mutually_exclusive
-        ),
-        cluster=dict(
-            type="dict", options=entity_by_spec, mutually_exclusive=mutually_exclusive
-        ),
-        vcpus=dict(type="int"),
-        cores_per_vcpu=dict(type="int"),
-        memory_gb=dict(type="int"),
-        networks=dict(type="list", elements="dict", options=network_spec),
-        boot_config=dict(type="dict", options=boot_config_spec),
-        guest_customization=dict(type="dict", options=gc_spec),
-        timezone=dict(type="str", default="UTC"),
-        categories=dict(type="dict"),
-    )
-
-    return module_args
-
-
 def clone_vm(module, result):
     vm_uuid = module.params["vm_uuid"]
 
@@ -126,7 +73,7 @@ def wait_for_task_completion(module, result, raise_error=True):
 
 
 def run_module():
-    module = BaseModule(argument_spec=get_module_spec(), supports_check_mode=True)
+    module = BaseModule(supports_check_mode=True)
     utils.remove_param_with_none_value(module.params)
     result = {
         "changed": False,
