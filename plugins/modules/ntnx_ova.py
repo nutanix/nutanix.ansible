ab58af50557049301b6d744321944e03a440c49b
alaa-bish <alaa.bishtawi@nutanix.com>
2022-04-11 10:39:14 +0300

    Fix integration test and sanity

--- plugins/modules/ntnx_ova.py
+++ plugins/modules/ntnx_ova.py
@@ -10,9 +10,9 @@ __metaclass__ = type
 DOCUMENTATION = r"""
 ---
 module: ntnx_ova
-short_description: VM module which supports VM CRUD operations
-version_added: 1.0.0
-description: "Create, Update, Delete, Power-on, Power-off Nutanix VM's"
+short_description: VM module which supports ova creation
+version_added: 1.0.2
+description: "Creates an ova entity"
 options:
   vm_uuid:
     description: VM UUID
@@ -41,9 +41,175 @@ author:
 """
 
 EXAMPLES = r"""
+- name: create_ova_image  with check mode
+  ntnx_ova:
+      vm_uuid: "{{ vm.vm_uuid }}"
+      name: integration_test_VMDK_ova
+      file_format: VMDK
+  register: result
+  ignore_errors: true
+  check_mode: yes
+
+- name: create QCOW2 ova_image
+  ntnx_ova:
+      vm_uuid: "{{ vm.vm_uuid }}"
+      name: integration_test_QCOW2_ova
+      file_format: QCOW2
+  register: result
+  ignore_errors: true
+
+- name: create VMDK ova_image
+  ntnx_ova:
+      vm_uuid: "{{ vm.vm_uuid }}"
+      name: integration_test_VMDK_ova
+      file_format: VMDK
+  register: result
+  ignore_errors: true
 """
 
 RETURN = r"""
+api_version:
+  description: API Version of the Nutanix v3 API framework.
+  returned: always
+  type: str
+  sample: "3.1"
+metadata:
+  description: The PBR metadata
+  returned: always
+  type: dict
+  sample: {
+    "categories": {},
+    "categories_mapping": {},
+    "creation_time": "2022-04-10T09:43:32Z",
+    "entity_version": "2",
+    "kind": "vm",
+    "last_update_time": "2022-04-10T09:43:32Z",
+    "owner_reference": {
+        "kind": "user",
+        "name": "admin",
+        "uuid": "00000000-0000-0000-0000-000000000000"
+    },
+    "project_reference": {
+        "kind": "project",
+        "name": "default",
+        "uuid": "37e22e5c-a914-4213-83e5-105123b8b5cf"
+    },
+    "spec_version": 0,
+    "uuid": "dded1b87-e566-419a-aac0-fb282792fb83"
+}
+spec:
+  description: An intentful representation of an ova spec
+  returned: always
+  type: dict
+  sample: {
+    "cluster_reference": {
+    "kind": "cluster",
+    "name": "auto_cluster_prod_4f4433c72b64",
+    "uuid": "0005dc0f-13a7-62e0-185b-ac1f6b6f97e2"
+},
+"name": "integration_test_ova_vm",
+"resources": {
+    "boot_config": {
+        "boot_device_order_list": [
+            "CDROM",
+            "DISK",
+            "NETWORK"
+        ],
+        "boot_type": "LEGACY"
+    },
+    "disk_list": [],
+    "gpu_list": [],
+    "hardware_clock_timezone": "UTC",
+    "is_agent_vm": false,
+    "machine_type": "PC",
+    "memory_size_mib": 4096,
+    "nic_list": [],
+    "num_sockets": 1,
+    "num_threads_per_core": 1,
+    "num_vcpus_per_socket": 1,
+    "power_state": "ON",
+    "power_state_mechanism": {
+        "guest_transition_config": {
+            "enable_script_exec": false,
+            "should_fail_on_script_failure": false
+        },
+        "mechanism": "HARD"
+    },
+    "serial_port_list": [],
+    "vga_console_enabled": true,
+    "vnuma_config": {
+        "num_vnuma_nodes": 0
+    }
+}
+}
+status:
+  description: An intentful representation of an ova status
+  returned: always
+  type: dict
+  sample: {
+                "cluster_reference": {
+                    "kind": "cluster",
+                    "name": "auto_cluster_prod_4f4433c72b64",
+                    "uuid": "0005dc0f-13a7-62e0-185b-ac1f6b6f97e2"
+                },
+                "execution_context": {
+                    "task_uuid": [
+                        "56471800-3702-4894-ab5d-b272f52f6352"
+                    ]
+                },
+                "name": "integration_test_ova_vm",
+                "resources": {
+                    "boot_config": {
+                        "boot_device_order_list": [
+                            "CDROM",
+                            "DISK",
+                            "NETWORK"
+                        ],
+                        "boot_type": "LEGACY"
+                    },
+                    "disk_list": [],
+                    "gpu_list": [],
+                    "hardware_clock_timezone": "UTC",
+                    "host_reference": {
+                        "kind": "host",
+                        "name": "10.46.136.28",
+                        "uuid": "c91be067-9885-4cbb-92cc-876242c0c396"
+                    },
+                    "hypervisor_type": "AHV",
+                    "is_agent_vm": false,
+                    "machine_type": "PC",
+                    "memory_size_mib": 4096,
+                    "nic_list": [],
+                    "num_sockets": 1,
+                    "num_threads_per_core": 1,
+                    "num_vcpus_per_socket": 1,
+                    "power_state": "ON",
+                    "power_state_mechanism": {
+                        "guest_transition_config": {
+                            "enable_script_exec": false,
+                            "should_fail_on_script_failure": false
+                        },
+                        "mechanism": "HARD"
+                    },
+                    "protection_type": "UNPROTECTED",
+                    "serial_port_list": [],
+                    "vga_console_enabled": true,
+                    "vnuma_config": {
+                        "num_vnuma_nodes": 0
+                    }
+                },
+                "state": "COMPLETE"
+            }
+vm_uuid:
+  description: The vm uuid
+  returned: always
+  type: str
+  sample: "64c5a93d-7cd4-45f9-81e9-e0b08d35077a"
+task_uuid:
+  description: The task uuid for the ove creation
+  returned: always
+  type: str
+  sample: "f83bbb29-3ca8-42c2-b29b-4fca4a7a25c3"
 """
 
 
--- plugins/modules/ntnx_vms_clone.py
+++ plugins/modules/ntnx_vms_clone.py
@@ -10,14 +10,14 @@ __metaclass__ = type
 DOCUMENTATION = r"""
 ---
 module: ntnx_vms_clone
-short_description: VM module which supports VM CRUD operations
-version_added: 1.0.0
-description: "Create, Update, Delete, Power-on, Power-off Nutanix VM's"
-
+short_description: VM module which supports VM clone operations
+version_added: 1.0.2
+description: "This creates a new vm by cloning the current vm "
 
 extends_documentation_fragment:
       - nutanix.ncp.ntnx_credentials
       - nutanix.ncp.ntnx_opperations
+      - nutanix.ncp.ntnx_vms_base
 author:
  - Prem Karat (@premkarat)
  - Gevorg Khachatryan (@Gevorg-Khachatryan-97)
@@ -25,6 +25,47 @@ author:
 """
 
 EXAMPLES = r"""
+- name: clone vm  with check mode
+  ntnx_vms_clone:
+      vm_uuid: "{{ vm.vm_uuid }}"
+      networks:
+        - is_connected: false
+          subnet:
+            name: "{{ network.dhcp.name }}"
+  register: result
+  ignore_errors: true
+  check_mode: yes
+
+- name: clone vm  and change vcpus,memory_gb,cores_per_vcpu,timezone,desc,name with force_power_off
+  ntnx_vms_clone:
+      vm_uuid: "{{ vm.vm_uuid }}"
+      vcpus: 2
+      cores_per_vcpu: 2
+      memory_gb: 2
+      name: cloned vm
+      desc: cloned vm
+      timezone: GMT
+      force_power_off: true
+
+- name: clone vm and add network
+  ntnx_vms_clone:
+      vm_uuid: "{{ vm.vm_uuid }}"
+      networks:
+        - is_connected: true
+          subnet:
+            uuid: "{{ network.dhcp.uuid }}"
+        - is_connected: true
+          subnet:
+            uuid: "{{ static.uuid }}"
+
+- name: clone vm  with script 
+  ntnx_vms_clone:
+      vm_uuid: "{{ vm.vm_uuid }}"
+      guest_customization:
+        type: "cloud_init"
+        script_path: "./cloud_init.yml"
+        is_overridable: True
+  register: result
 """
 
 RETURN = r"""
