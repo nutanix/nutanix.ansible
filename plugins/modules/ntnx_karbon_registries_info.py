1447064a2b506a5763abf5d9c6627b40e3f4acff
Gevorg-Khachatryaan <Gevorg1050.1>
2022-08-29 12:37:39 +0400

    registry info module

--- plugins/modules/ntnx_karbon_registries_info.py
+++ plugins/modules/ntnx_karbon_registries_info.py
@@ -9,7 +9,7 @@ __metaclass__ = type
 
 DOCUMENTATION = r"""
 ---
-module: ntnx_karbon_registrys_info
+module: ntnx_karbon_registries_info
 short_description: registry  info module
 version_added: 1.5.0
 description: 'Get registry info'
@@ -18,12 +18,6 @@ options:
         description:
             - registry name
         type: str
-      fetch_ssh_credentials:
-        type: bool
-        description: write
-      fetch_kubeconfig:
-        type: bool
-        description: write
 extends_documentation_fragment:
       - nutanix.ncp.ntnx_credentials
 author:
@@ -32,16 +26,16 @@ author:
  - Alaa Bishtawi (@alaa-bish)
 """
 EXAMPLES = r"""
-  - name: List registrys
-    ntnx_karbon_registrys_info:
+  - name: List registries
+    ntnx_karbon_registries_info:
       nutanix_host: "{{ ip }}"
       nutanix_username: "{{ username }}"
       nutanix_password: "{{ password }}"
       validate_certs: False
     register: result
 
-  - name: Get registrys using name
-    ntnx_registrys_info:
+  - name: Get registries using name
+    ntnx_registries_info:
       nutanix_host: "{{ ip }}"
       nutanix_username: "{{ username }}"
       nutanix_password: "{{ password }}"
@@ -49,62 +43,8 @@ EXAMPLES = r"""
       registry_name: "registry-name"
     register: result
 
-  - name:  Get registrys with ssh credential
-    ntnx_karbon_registrys_info:
-      nutanix_host: "{{ ip }}"
-      nutanix_username: "{{ username }}"
-      nutanix_password: "{{ password }}"
-      validate_certs: False
-      registry_name: "registry-name"
-      fetch_ssh_credentials: true
-    register: result
-
-  - name:  Get registrys with kubeconfig
-    ntnx_karbon_registrys_info:
-      nutanix_host: "{{ ip }}"
-      nutanix_username: "{{ username }}"
-      nutanix_password: "{{ password }}"
-      validate_certs: False
-      registry_name: "registry-name"
-      fetch_kubeconfig: true
-    register: result
 """
 RETURN = r"""
-cni_config:
-  description: Container networking interface (CNI) information.
-  returned: always
-  type: dict
-  sample:
-    {
-                "flannel_config": null,
-                "node_cidr_mask_size": 24,
-                "pod_ipv4_cidr": "172.20.0.0/16",
-                "service_ipv4_cidr": "172.19.0.0/16"
-            }
-etcd_config:
-  description: Etcd configuration information.
-  returned: always
-  type: dict
-  sample: {
-                "node_pools": [
-                    "test-module21_etcd_pool"
-                ]
-            }
-kubeapi_server_ipv4_address:
-    description: IPV4 address of the API server.
-    returned: always
-    type: str
-    sample: "10.44.78.171"
-master_config:
-    description: Configuration of master nodes.
-    returned: always
-    type: dict
-    sample: {
-                "deployment_type": "single-master",
-                "node_pools": [
-                    "test-module21_master_pool"
-                ]
-            }
 name:
     description: K8s registry name.
     returned: always
@@ -120,42 +60,6 @@ uuid:
     returned: always
     type: str
     sample: "00000000-0000-0000-0000-000000000000"
-version:
-    description: K8s version of the registry.
-    returned: always
-    type: str
-    sample: "1.19.8-0"
-worker_config:
-    description: Worker configuration information.
-    returned: always
-    type: dict
-    sample: {
-                "node_pools": [
-                    "test-module21_worker_pool"
-                ]
-            }
-kube_config:
-    description: write
-    returned: if fetch_kubeconfig is true
-    type: str
-certificate:
-    description: ssh certifcate
-    returned: if fetch_ssh_credentials is true
-    type: str
-expiry_time:
-    description: expire time for certificate
-    returned: if fetch_ssh_credentials is true
-    type: str
-    sample: "2022-08-16T06:33:18.000Z"
-private_key:
-    description: user private key
-    returned: if fetch_ssh_credentials is true
-    type: str
-username:
-    description: name
-    returned: if fetch_ssh_credentials is true
-    type: str
-    sample: admin
 """
 
 from ..module_utils.base_info_module import BaseInfoModule  # noqa: E402
@@ -193,11 +97,6 @@ def run_module():
         argument_spec=get_module_spec(),
         supports_check_mode=False,
         skip_info_args=True,
-        mutually_exclusive=[("fetch_ssh_credentials", "fetch_kubeconfig")],
-        required_if=[
-            ("fetch_ssh_credentials", True, ("registry_name",)),
-            ("fetch_kubeconfig", True, ("registry_name",)),
-        ],
     )
     result = {"changed": False, "error": None, "response": None}
     if module.params.get("registry_name"):
