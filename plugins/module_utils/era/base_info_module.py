5454f43ce96c8abdcd3610bc00d9daa47fff5c65
Gevorg-Khachatryaan <Gevorg1050.1>
2022-09-26 12:23:06 +0400

    era db info modules

--- plugins/module_utils/era/base_info_module.py
+++ plugins/module_utils/era/base_info_module.py
@@ -2,16 +2,13 @@
 # Simplified BSD License (see licenses/simplified_bsd.txt or https://opensource.org/licenses/BSD-2-Clause )
 from __future__ import absolute_import, division, print_function
 
-from copy import deepcopy
-
-from plugins.module_utils.base_info_module import BaseInfoModule
+from ..base_info_module import BaseInfoModule
 
 __metaclass__ = type
 
 
 class BaseEraInfoModule(BaseInfoModule):
 
-    def __init__(self, skip_info_args=False, **kwargs):
-        self.argument_spec = deepcopy(BaseInfoModule.argument_spec)
-        self.argument_spec.pop("nutanix_port")
+    def __init__(self, **kwargs):
         super(BaseEraInfoModule, self).__init__(**kwargs)
+        self.argument_spec.pop("nutanix_port")
