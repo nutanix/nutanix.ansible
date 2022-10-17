1d9ef4e02bb8e4837ac2e207e47f58e5bf21c7e5
Pradeepsingh Bhati <pradeep.bhati@nutanix.com>
2022-10-17 13:34:01 +0530

    Minor name changes

---
+++ plugins/module_utils/ndb/clones.py
@@ -0,0 +1,13 @@
+# This file is part of Ansible
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+
+from .era import Era
+
+
+class Clone(Era):
+    def __init__(self, module, resource_type="/v0.9/clones"):
+        super(Clone, self).__init__(module, resource_type=resource_type)
