70c4f5a3a38df67371669dd9188e90be1b30490e
Gevorg-Khachatryaan <Gevorg1050.1>
2022-10-17 12:31:51 +0400

    volume groups module

---
+++ plugins/module_utils/prism/volume_groups.py
@@ -0,0 +1,93 @@
+# This file is part of Ansible
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+from copy import deepcopy
+
+from .clusters import get_cluster_uuid
+from .prism import Prism
+
+
+class VolumeGroup(Prism):
+    __BASEURL__ = "/api/storage/v4.0.a2/config"
+
+    def __init__(self, module):
+        resource_type = "/volume-groups"
+        super(VolumeGroup, self).__init__(module, resource_type=resource_type)
+        self.build_spec_methods = {
+            "name": self._build_spec_name,
+            "desc": self._build_spec_desc,
+            "cluster": self._build_spec_cluster,
+            "target_prefix": self._build_spec_target_prefix,
+            "load_balance": self._build_spec_load_balance,
+        }
+
+    # def get_uuid(self, value, key="name", raise_error=True, no_response=False):
+    #     data = {"filter": "{0}=={1}".format(key, value)}
+    #     resp = self.list(data, raise_error=raise_error, no_response=no_response)
+    #     entities = resp.get("entities") if resp else None
+    #     if entities:
+    #         for entity in entities:
+    #             if entity["service_group"]["name"] == value:
+    #                 return entity["uuid"]
+    #     return None
+
+    def _get_default_spec(self):
+        return deepcopy(
+            {
+                "name": "",
+                "description": "",
+                "loadBalanceVmAttachments": False,
+                "sharingStatus": "SHARED",
+                "iscsiTargetPrefix": "",
+                "clusterReference": None,
+                "usageType": "USER",
+            }
+        )
+
+    def _build_spec_name(self, payload, value):
+        payload["name"] = value
+        return payload, None
+
+    def _build_spec_desc(self, payload, value):
+        payload["description"] = value
+        return payload, None
+
+    def _build_spec_target_prefix(self, payload, value):
+        payload["iscsiTargetPrefix"] = value
+        return payload, None
+
+    def _build_spec_load_balance(self, payload, value):
+        payload["loadBalanceVmAttachments"] = value
+        return payload, None
+
+    def _build_spec_cluster(self, payload, param):
+        uuid, err = get_cluster_uuid(param, self.module)
+        if err:
+            return None, err
+        payload["clusterReference"] = uuid
+        return payload, None
+
+# Helper functions
+
+
+class VGDisks():
+    pass
+
+def get_volume_group_uuid(config, module):
+    if "name" in config:
+        service_group = VolumeGroup(module)
+        name = config["name"]
+        uuid = service_group.get_uuid(name)
+        if not uuid:
+            error = "Volume Group {0} not found.".format(name)
+            return None, error
+    elif "uuid" in config:
+        uuid = config["uuid"]
+    else:
+        error = "Config {0} doesn't have name or uuid key".format(config)
+        return None, error
+
+    return uuid, None
---
+++ plugins/modules/ntnx_volume_groups.py
@@ -0,0 +1,208 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2021, Prem Karat
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+---
+module: ntnx_volume_groups
+short_description: volume_groups module which suports volume_groups CRUD operations
+version_added: 1.4.0
+description: 'Create, Update, Delete volume_group'
+options:
+  state:
+    description:
+      - Specify state of volume_groups
+      - If C(state) is set to C(present) then volume_groups is created.
+      - >-
+        If C(state) is set to C(absent) and if the volume_groups exists, then
+        volume_groups is removed.
+    choices:
+      - present
+      - absent
+    type: str
+    default: present
+  wait:
+    description: Wait for volume_groups CRUD operation to complete.
+    type: bool
+    required: false
+    default: True
+  name:
+    description: volume_groups Name
+    required: False
+    type: str
+  volume_group_uuid:
+    description: volume_group UUID
+    type: str
+  desc:
+    description: volume_groups description
+    type: str
+  
+  ...............TODO
+  
+extends_documentation_fragment:
+      - nutanix.ncp.ntnx_credentials
+      - nutanix.ncp.ntnx_operations
+author:
+  - Prem Karat (@premkarat)
+  - Gevorg Khachatryan (@Gevorg-Khachatryan-97)
+  - Alaa Bishtawi (@alaa-bish)
+"""
+
+EXAMPLES = r"""
+
+"""
+
+RETURN = r"""
+
+"""
+
+from ..module_utils.base_module import BaseModule  # noqa: E402
+from ..module_utils.prism.volume_groups import VolumeGroup  # noqa: E402
+from ..module_utils.utils import remove_param_with_none_value  # noqa: E402
+
+
+def get_module_spec():
+    mutually_exclusive = [("name", "uuid")]
+
+    entity_by_spec = dict(name=dict(type="str"), uuid=dict(type="str"))
+
+    disk_spec = dict(
+        size_gb=dict(type="int"),
+        storage_container=dict(
+            type="dict", options=entity_by_spec, mutually_exclusive=mutually_exclusive
+        ),
+    )
+
+    module_args = dict(
+        name=dict(type="str"),
+        desc=dict(type="str"),
+        cluster=dict(
+            type="dict", options=entity_by_spec, mutually_exclusive=mutually_exclusive
+        ),
+        target_prefix=dict(type="str"),
+        volume_group_uuid=dict(type="str"),
+        flash_mode=dict(type="bool", default=False),
+        disks=dict(type="list", elements="dict", options=disk_spec),
+        vms=dict(type="list", elements="str"),
+        load_balance=dict(type="bool", default=False),
+        clients=dict(type="list", elements="str"),
+        CHAP_auth=dict(type="bool", default=False),
+        target_password=dict(type="str", no_log=True),
+
+    )
+
+    return module_args
+
+
+def create_volume_group(module, result):
+    volume_group = VolumeGroup(module)
+    spec, error = volume_group.get_spec()
+    if error:
+        result["error"] = error
+        module.fail_json(msg="Failed generating volume_groups spec", **result)
+
+    if module.check_mode:
+        result["response"] = spec
+        return
+
+    # attach disks
+    # VGDisks
+    # attach vms
+
+    # attach clients
+
+    resp = volume_group.create(spec)
+    # volume_group_uuid = resp["uuid"]
+    result["changed"] = True
+    result["response"] = resp
+    # result["volume_group_uuid"] = volume_group_uuid
+
+
+# def update_volume_group(module, result):
+#     volume_group = VolumeGroup(module)
+#     volume_group_uuid = module.params.get("volume_group_uuid")
+#     if not volume_group_uuid:
+#         result["error"] = "Missing parameter volume_group_uuid in playbook"
+#         module.fail_json(msg="Failed updating volume_group", **result)
+#     result["volume_group_uuid"] = volume_group_uuid
+#
+#     # read the current state of volume_group
+#     resp = volume_group.read(volume_group_uuid)
+#     resp = resp.get("volume_group")
+#
+#     # new spec for updating volume_group
+#     update_spec, error = volume_group.get_spec(resp)
+#     if error:
+#         result["error"] = error
+#         module.fail_json(msg="Failed generating volume_group update spec", **result)
+#
+#     # check for idempotency
+#     if resp == update_spec:
+#         result["skipped"] = True
+#         module.exit_json(
+#             msg="Nothing to change. Refer docs to check for fields which can be updated"
+#         )
+#
+#     if module.check_mode:
+#         result["response"] = update_spec
+#         return
+#
+#     # update volume_group
+#     volume_group.update(update_spec, uuid=volume_group_uuid, no_response=True)
+#
+#     resp = volume_group.read(volume_group_uuid)
+#
+#     result["changed"] = True
+#     result["response"] = resp
+
+
+# def delete_volume_group(module, result):
+#     volume_group_uuid = module.params["volume_group_uuid"]
+#     if not volume_group_uuid:
+#         result["error"] = "Missing parameter volume_group_uuid in playbook"
+#         module.fail_json(msg="Failed deleting volume_groups", **result)
+#
+#     volume_group = VolumeGroup(module)
+#     resp = volume_group.delete(volume_group_uuid, no_response=True)
+#     result["changed"] = True
+#     result["response"] = resp
+#     result["volume_group_uuid"] = volume_group_uuid
+
+
+def run_module():
+    module = BaseModule(
+        argument_spec=get_module_spec(),
+        supports_check_mode=True,
+        required_if=[
+            ("state", "present", ("name", "volume_group_uuid"), True),
+            ("state", "absent", ("volume_group_uuid",)),
+        ],
+    )
+    remove_param_with_none_value(module.params)
+    result = {
+        "changed": False,
+        "error": None,
+        "response": None,
+        "volume_group_uuid": None,
+    }
+    state = module.params["state"]
+    if state == "absent":
+        delete_volume_group(module, result)
+    elif module.params.get("volume_group_uuid"):
+        update_volume_group(module, result)
+    else:
+        create_volume_group(module, result)
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == "__main__":
+    main()
