c221ce12089a4bfdca43e22cc7d824ca53663b42
Pradeepsingh Bhati <pradeep.bhati@nutanix.com>
2022-10-31 19:09:08 +0530

    database restore spec design

---
+++ plugins/modules/ntnx_ndb_databases_restore.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright: (c) 2021, Prem Karat
+# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+DOCUMENTATION = r"""
+"""
+
+EXAMPLES = r"""
+"""
+
+RETURN = r"""
+"""
+import time  # noqa: E402
+
+from ..module_utils.ndb.base_module import NdbBaseModule  # noqa: E402
+from ..module_utils.ndb.databases import Database  # noqa: E402
+from ..module_utils.ndb.operations import Operation  # noqa: E402
+from ..module_utils.utils import remove_param_with_none_value  # noqa: E402
+
+
+def get_module_spec():
+
+    module_args = dict(
+        snapshot_uuid=dict(type="str", required=False),
+        pitr_timestamp=dict(type="str", required=False),
+        db_uuid=dict(type="str", required=True),
+        time_zone=dict(type="str", required=False),
+    )
+    return module_args
+
+
+# restore database
+def restore_database(module, result):
+    pass
+
+
+def run_module():
+    module = NdbBaseModule(
+        argument_spec=get_module_spec(),
+        supports_check_mode=True,
+        mutually_exclusive=[("snapshot_uuid", "pitr_timestamp")],
+        required_if=[("state", "present", ("snapshot_uuid", "pitr_timestamp"), True)],
+    )
+    remove_param_with_none_value(module.params)
+    result = {"changed": False, "error": None, "response": None}
+
+    restore_database(module, result)
+
+    module.exit_json(**result)
+
+
+def main():
+    run_module()
+
+
+if __name__ == "__main__":
+    main()
