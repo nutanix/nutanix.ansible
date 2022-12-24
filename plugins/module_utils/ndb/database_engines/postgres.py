1368be7a99fc61caf4d8bd2154647b96fc9a779d
Pradeepsingh Bhati <pradeep.bhati@nutanix.com>
2022-12-25 00:42:20 +0530

    Minor code changes

---
+++ plugins/module_utils/ndb/database_engines/postgres.py
@@ -0,0 +1,222 @@
+from copy import deepcopy
+
+from database_engine import DatabaseEngine
+from ..clusters import get_cluster_uuid
+
+
+class Postgres(DatabaseEngine):
+    def __init__(self, module):
+        self._type = "postgres"
+        super(Postgres, self).__init__(module)
+
+    def build_spec_db_params_profile_properties(self, params, cur_properties=None):
+        input_properties = deepcopy(params)
+        properties = []
+
+        # for update
+        if cur_properties:
+            properties = deepcopy(properties)
+
+        # map of properties with defaults
+        default_properties = {
+            "max_connections": 100,
+            "max_replication_slots": 10,
+            "effective_io_concurrency": 1,
+            "timezone": "UTC",
+            "max_prepared_transactions": 0,
+            "max_locks_per_transaction": 64,
+            "max_wal_senders": 10,
+            "max_worker_processes": 8,
+            "checkpoint_completion_target": 0.5,
+            "autovacuum": "on",
+            "autovacuum_freeze_max_age": 200000000,
+            "autovacuum_vacuum_threshold": 50,
+            "autovacuum_vacuum_scale_factor": 0.2,
+            "autovacuum_work_mem": -1,
+            "autovacuum_max_workers": 3,
+            "wal_buffers": -1,
+            "synchronous_commit": "on",
+            "random_page_cost": 4,
+            "wal_keep_segments": 700,
+            "min_wal_size": 80,
+            "max_wal_size": 1,
+            "checkpoint_timeout": 5,
+            "autovacuum_vacuum_cost_delay": 2,
+        }
+
+        # map of certain properties with their units
+        property_units = {
+            "min_wal_size": "MB",
+            "max_wal_size": "GB",
+            "checkpoint_timeout": "min",
+            "autovacuum_vacuum_cost_delay": "ms",
+        }
+
+        # update current properties with input config
+        if properties:
+
+            for prop in properties:
+                # if input given
+                if prop["name"] in input_properties:
+
+                    # get property value from input and add unit suffix if required
+                    val = str(input_properties[prop["name"]]) + property_units.get(
+                        prop["name"], ""
+                    )
+
+                    # update the property value
+                    prop["value"] = val
+
+        else:
+
+            for name, val in default_properties:
+
+                # check if property value is given in input
+                if name in input_properties:
+                    val = str(val)
+
+                val = val + property_units.get(name, "")
+
+                spec = {"name": name, "value": val}
+                properties.append(spec)
+
+        return properties, None
+
+
+class PostgresSingleInstance(Postgres):
+    def __init__(self, module):
+        super(Postgres, self).__init__(module)
+
+    def build_spec_db_instance_action_arguments(self, payload, config):
+
+        action_arguments = payload.get("actionArguments", [])
+        # fields to their defaults maps
+        args = {
+            "listener_port": "",
+            "auto_tune_staging_drive": False,
+            "allocate_pg_hugepage": False,
+            "cluster_database": False,
+            "auth_method": "",
+            "db_password": "",
+            "pre_create_script": "",
+            "post_create_script": "",
+        }
+
+        # create action arguments
+        for key, value in args.items():
+            spec = {"name": key, "value": config.get(key, value)}
+            action_arguments.append(spec)
+
+        # handle scenariors where display names are diff
+        action_arguments.append(
+            {"name": "database_names", "value": action_arguments.get("db_name")}
+        )
+        action_arguments.append(
+            {"name": "database_size", "value": str(action_arguments.get("db_size"))}
+        )
+
+        payload["actionArguments"] = action_arguments
+        return payload, None
+
+
+class PostgresHAInstance(Postgres):
+    def __init__(self, module):
+        super(Postgres, self).__init__(module)
+
+    def build_spec_db_instance_action_arguments(self, payload, config):
+
+        action_arguments = payload.get("actionArguments", [])
+        # Below is a map of common action arguments fields to their default value
+        args = {
+            "listener_port": "",
+            "allocate_pg_hugepage": False,
+            "cluster_database": False,
+            "db_password": "",
+            "pre_create_script": "",
+            "post_create_script": "",
+            "patroni_cluster_name": "",
+            "backup_policy": "",
+            "failover_mode": "",
+            "archive_wal_expire_days": "",
+            "enable_synchronous_mode": False,
+            "enable_peer_auth": False,
+            "node_type": "database",
+        }
+
+        # create action arguments
+        for key, default in args.items():
+            spec = {"name": key, "value": config.get(key, default)}
+            action_arguments.append(spec)
+
+        # handle scenariors where display names are different
+        action_arguments.append(
+            {"name": "database_names", "value": config.get("db_name")}
+        )
+        action_arguments.append(
+            {"name": "database_size", "value": str(params.get("db_size"))}
+        )
+
+        # for HA instance, add HA proxy related action arguments and vm details if required
+        ha_proxy = config.get("ha_proxy")
+        if ha_proxy:
+            action_arguments, err = self._build_spec_ha_proxy_action_arguments(
+                ha_proxy, action_arguments=action_arguments
+            )
+            if err:
+                return None, err
+
+        payload["actionArguments"] = action_arguments
+        return payload, None
+
+    def build_spec_ha_proxy_vm(self, ha_proxy, payload=None):
+
+        if not payload:
+            payload = {}
+
+        # validations
+        if not ha_proxy.get("cluster"):
+            return None, "cluster info is required for ha proxy"
+
+        if not ha_proxy.get("name"):
+            return None, "name is required for ha proxy"
+
+        # add HA proxy vm details
+        nodes = payload.get("nodes", [])
+        node_spec = {"properties": [{"name": "node_type", "value": "haproxy"}]}
+
+        uuid, err = get_cluster_uuid(self.module, ha_proxy.get("cluster"))
+        if err:
+            return None, err
+
+        node_spec["nxClusterId"] = uuid
+        node_spec["vmName"] = ha_proxy["name"]
+        nodes.append(node_spec)
+
+        payload["nodes"] = nodes
+        payload["nodeCount"] = len(payload["nodes"])
+        return payload, None
+
+    def build_spec_ha_proxy_action_arguments(self, ha_proxy, action_arguments=None):
+
+        if not action_arguments:
+            action_arguments = []
+
+        action_arguments.append(
+            {
+                "name": "provision_virtual_ip",
+                "value": ha_proxy.get("provision_virtual_ip", False),
+            }
+        )
+
+        if ha_proxy.get("write_port"):
+            action_arguments.append(
+                {"name": "proxy_write_port", "value": ha_proxy["write_port"]}
+            )
+
+        if ha_proxy.get("read_port"):
+            action_arguments.append(
+                {"name": "proxy_read_port", "value": ha_proxy["read_port"]}
+            )
+
+        action_arguments.append({"name": "deploy_haproxy", "value": True})
+        return action_arguments, None
--- plugins/module_utils/ndb/db_servers.py
+++ plugins/module_utils/ndb/db_servers.py
@@ -1,6 +1,11 @@
 # This file is part of Ansible
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 from __future__ import absolute_import, division, print_function
+from copy import deepcopy
+
+from clusters import get_cluster_uuid
+from database_instance import DatabaseInstance
+from profiles import Profile, get_profile_uuid
 
 __metaclass__ = type
 
@@ -8,10 +13,17 @@ __metaclass__ = type
 from .nutanix_database import NutanixDatabase
 
 
-class DBServers(NutanixDatabase):
+class DBServerVM(NutanixDatabase):
+
+    db_instance: DatabaseInstance = None
+
     def __init__(self, module):
         resource_type = "/dbservers"
-        super(DBServers, self).__init__(module, resource_type=resource_type)
+        super(DBServerVM, self).__init__(module, resource_type=resource_type)
+
+    def set_database_instance(self, db_instance: DatabaseInstance):
+        self.db_instance = db_instance
+        return self.db_instance
 
     def get_uuid(
         self,
@@ -51,13 +63,149 @@ class DBServers(NutanixDatabase):
 
         return resp, None
 
+    def provision(self, data):
+        pass
 
-# Helper functions
+    def register(self, data):
+        pass
+
+    def get_default_payload(self):
+        return deepcopy(
+            {
+                "actionArguments": [],
+                "nxClusterId": "",
+                "databaseType": "",
+                "latestSnapshot": True,
+                "networkProfileId": "",
+                "softwareProfileId": "",
+                "softwareProfileVersionId": "",
+                "computeProfileId": "",
+                "vmPassword": None,
+            }
+        )
+
+    # high level builders as per use case
+    def build_spec_provision_for_instance(self, payload, vm_config):
+        build_spec_methods = {
+            "name": self._build_spec_name,
+            "compute_profile": self._build_spec_compute_profile,
+            "software_profile": self._build_spec_software_profile,
+            "network_profile": self._build_spec_network_profile,
+            "cluster": self._build_spec_cluster,
+            "pub_ssh_key": self._build_spec_pub_ssh_key,
+            "vm_password": self._build_spec_vm_password,
+        }
+
+        for field, method in build_spec_methods.items():
+            if field in vm_config:
+                payload, err = method(payload, vm_config[field])
+                if err:
+                    return None, err
+
+        payload["nodeCount"] = 1
+        payload["clustered"] = False
+        return payload, err
+
+    def build_spec_provision(self, payload, vm_config):
+        build_spec_methods = {
+            "compute_profile": self._build_spec_compute_profile,
+            "software_profile": self._build_spec_software_profile,
+            "network_profile": self._build_spec_network_profile,
+            "cluster": self._build_spec_cluster,
+            "vm_password": self._build_spec_vm_password,
+            "database_type": self._build_spec_database_type,
+        }
+
+        for field, method in build_spec_methods.items():
+            if field in vm_config:
+                payload, err = method(payload, vm_config[field])
+                if err:
+                    return None, err
+
+        if not payload.get("actionArguments"):
+            payload["actionArguments"] = []
+
+        payload["actionArguments"].append(
+            {"name": "vm_name", "value": vm_config.get("name")}
+        )
+
+        payload["actionArguments"].append(
+            {"name": "client_public_key", "value": vm_config.get("pub_ssh_key")}
+        )
+
+        return payload, err
+
+    def build_spec_register(self, payload, params):
+        pass
+
+    # builder methods for vm
+    def _build_spec_name(self, payload, name):
+        if not payload.get("nodes"):
+            payload["nodes"] = [{}]
+
+        payload["nodes"][0]["vmName"] = name
+        return payload, None
+
+    def _build_spec_compute_profile(self, payload, profile):
+        # set compute profile
+        uuid, err = get_profile_uuid(self.module, "Compute", profile)
+        if err:
+            return None, err
+        payload["computeProfileId"] = uuid
+        return payload, None
+
+    def _build_spec_software_profile(self, payload, profile):
+        # set software profile
+        uuid, err = get_profile_uuid(self.module, "Software", profile)
+        if err:
+            return None, err
+
+        payload["softwareProfileId"] = uuid
+        if profile["software_profile"].get("version_id"):
+            payload["softwareProfileVersionId"] = profile["software_profile"][
+                "version_id"
+            ]
+        else:
+            profiles = Profile(self.module)
+            software_profile = profiles.read(uuid)
+            payload["softwareProfileId"] = uuid
+            payload["softwareProfileVersionId"] = software_profile["latestVersionId"]
+
+        return payload, None
+
+    def _build_spec_network_profile(self, payload, profile):
+        # set network prfile
+        uuid, err = get_profile_uuid(self.module, "Network", profile)
+        if err:
+            return None, err
+
+        payload["networkProfileId"] = uuid
+        return payload, None
+
+    def _build_spec_cluster(self, payload, cluster):
+        # set cluster config
+        uuid, err = get_cluster_uuid(self.module, cluster["cluster"])
+        if err:
+            return None, err
+        payload["nxClusterId"] = uuid
+        return payload, None
+
+    def _build_spec_pub_ssh_key(self, payload, pub_ssh_key):
+        payload["sshPublicKey"] = pub_ssh_key
+        return payload, None
+
+    def _build_spec_vm_password(self, payload, password):
+        payload["vmPassword"] = password
+        return payload, None
+
+    def _build_spec_database_type(self, payload, db_type):
+        payload["database_type"] = db_type
+        return payload, None
 
 
 def get_db_server_uuid(module, config):
     if "name" in config:
-        db_servers = DBServers(module)
+        db_servers = DBServerVM(module)
         name = config["name"]
         uuid, err = db_servers.get_uuid(name)
         if err:
