32d11442df06363a56244e576bee374cd497d748
alaa-bish <alaa.bishtawi@nutanix.com>
2023-02-08 16:06:26 +0200

    prepare doc structure

--- plugins/modules/ntnx_ndb_register_database.py
+++ plugins/modules/ntnx_ndb_register_database.py
@@ -19,8 +19,200 @@ options:
             - write
         type: str
         required: true
-
-
+    desc:
+        description:
+            - write
+        type: str
+    db_vm:
+        description:
+            - write
+        type: dict
+        required: true
+        suboptions:
+            registered:
+                description:
+                    - write
+                type: dict
+                suboptions:
+                    name:
+                        description:
+                            - write
+                        type: str
+                    uuid:
+                        description:
+                            - write
+                        type: str
+                    ip:
+                        description:
+                            - write
+                        type: str
+            unregistered:
+                description:
+                    - write
+                type: dict
+                suboptions:
+                    ip:
+                        description:
+                            - write
+                        type: str
+                        required: true
+                    username:
+                        description:
+                            - write
+                        type: str
+                        required: true
+                    private_key:
+                        description:
+                            - write
+                        type: str
+                    password:
+                        description:
+                            - write
+                        type: str
+                    desc:
+                        description:
+                            - write
+                        type: str
+                    reset_desc_in_ntnx_cluster:
+                        description:
+                            - write
+                        type: bool
+                        default: false
+                    cluster:
+                        description:
+                            - write
+                        type: dict
+                        required: true
+                        suboptions:
+                            name:
+                                description:
+                                    - write
+                                type: str
+                            uuid:
+                                description:
+                                    - write
+                                type: str
+    time_machine:
+        description:
+            - write
+        type: dict
+        required: true
+        suboptions:
+            name:
+                description:
+                    - write
+                type: str
+                required: true
+            desc:
+                description:
+                    - write
+                type: str
+            sla:
+                description:
+                    - write
+                type: dict
+                required: true
+                suboptions:
+                    name:
+                        description:
+                            - write
+                        type: str
+                    uuid:
+                        description:
+                            - write
+                        type: str
+            schedule:
+                description:
+                    - write
+                type: dict
+                suboptions:
+                    daily:
+                        description:
+                            - write
+                        type: str
+                    weekly:
+                        description:
+                            - write
+                        type: str
+                    monthly:
+                        description:
+                            - write
+                        type: int
+                    quaterly:
+                        description:
+                            - write
+                        type: str
+                    yearly:
+                        description:
+                            - write
+                        type: str
+                    log_catchup:
+                        description:
+                            - write
+                        type: int
+                        choices: [15, 30, 60, 90, 120]
+                    snapshots_per_day:
+                        description:
+                            - write
+                        type: int
+                        default: 1
+            auto_tune_log_drive:
+                description:
+                    - write
+                type: bool
+                default: true
+    postgres:
+        description:
+            - write
+        type: dict
+        suboptions:
+            listener_port:
+                description:
+                    - write
+                type: str
+                default: "5432"
+            db_name:
+                description:
+                    - write
+                type: str
+                required: true
+            db_password:
+                description:
+                    - write
+                type: str
+                required: true
+            db_user:
+                description:
+                    - write
+                type: str
+                default: "postgres"
+            software_path:
+                description:
+                    - write
+                type: str
+            type:
+                description:
+                    - write
+                type: str
+                choices: ["single"]
+                default: "single"
+    tags:
+        description:
+            - write
+        type: dict
+    auto_tune_staging_drive:
+        description:
+            - write
+        type: bool
+    working_directory:
+        description:
+            - write
+        type: str
+        default: "/tmp"
+    automated_patching:
+        description:
+            - write
+        type: dict
 
 extends_documentation_fragment:
       - nutanix.ncp.ntnx_ndb_base_module
@@ -64,8 +256,8 @@ def get_module_spec():
     unregistered_vm = dict(
         ip=dict(type="str", required=True),
         username=dict(type="str", required=True),
-        private_key=dict(type="str", required=False),
-        password=dict(type="str", required=False),
+        private_key=dict(type="str", required=False, no_log=True),
+        password=dict(type="str", required=False, no_log=True),
         desc=dict(type="str", required=False),
         reset_desc_in_ntnx_cluster=dict(type="bool", default=False, required=False),
         cluster=dict(
@@ -125,7 +317,7 @@ def get_module_spec():
         db_password=dict(type="str", required=True, no_log=True),
         db_user=dict(type="str", default="postgres", required=False),
         software_path=dict(type="str", required=False),
-        type=dict(dict="str", choices=["single"], default="single", required=False),
+        type=dict(type="str", choices=["single"], default="single", required=False),
     )
 
     module_args = dict(
