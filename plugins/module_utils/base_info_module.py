d1c75d103861d3d4380fc1a98278bb8b1b4b0a2c
Gevorg-Khachatryaan <Gevorg1050.1>
2023-10-19 13:54:46 +0400

    fiql string in filtering

--- plugins/module_utils/base_info_module.py
+++ plugins/module_utils/base_info_module.py
@@ -15,7 +15,12 @@ class BaseInfoModule(BaseModule):
         length=dict(type="int"),
         filter=dict(type="dict"),
         custom_filter=dict(type="dict"),
+        filter_string=dict(type="str"),
     )
+
+    info_args_mutually_exclusive = [
+        ("filter", "filter_string"),
+    ]
 
     def __init__(self, skip_info_args=False, **kwargs):
         self.argument_spec = deepcopy(BaseModule.argument_spec)
@@ -23,4 +28,8 @@ class BaseInfoModule(BaseModule):
         self.argument_spec.pop("wait")
         if not skip_info_args:
             self.argument_spec.update(self.info_argument_spec)
+            info_args_mutually_exclusive = deepcopy(self.info_args_mutually_exclusive)
+            if kwargs.get("mutually_exclusive"):
+                info_args_mutually_exclusive.append(kwargs["mutually_exclusive"])
+            kwargs["mutually_exclusive"] = info_args_mutually_exclusive
         super(BaseInfoModule, self).__init__(**kwargs)
