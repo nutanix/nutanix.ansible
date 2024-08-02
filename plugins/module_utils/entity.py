4588d26f5a7a750a6b9e5a408c9d74ddef00f82e
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2024-08-02 13:46:18 +0000

    revert  msg length check
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- plugins/module_utils/entity.py
+++ plugins/module_utils/entity.py
@@ -412,7 +412,7 @@ class Entity(object):
 
         if resp_json is None:
             try:
-                if len(info.get("msg")) > 2:
+                if len(info.get("msg")) > 0:
                     resp_json_msg = "{}".format(info.get("msg"))
                 else:
                     resp_json_msg = "Failed to convert API response to json"
