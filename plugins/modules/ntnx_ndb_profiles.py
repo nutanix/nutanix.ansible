6d971881326d13679bc741f5250400487c0ac56b
george-ghawali <george.ghawali@nutanix.com>
2024-09-26 16:24:54 +0300

    Adding more spelling fixes

--- plugins/modules/ntnx_ndb_profiles.py
+++ plugins/modules/ntnx_ndb_profiles.py
@@ -311,7 +311,7 @@ options:
                             autovacuum_max_workers:
                                 description:
                                     - autovacuum max workers
-                                    - default is 3
+                                    - deadefaultfult is 3
                                 type: int
                             autovacuum_vacuum_cost_delay:
                                 description:
@@ -1081,7 +1081,7 @@ def create_profile(module, result):
     result["response"] = resp
     uuid = resp.get("id")
 
-    # in case there is process of replication triggered, operation info is received
+    # incase there is process of replication triggered, operation info is received
     if profile_type == "software" and not uuid:
         uuid = resp.get("entityId")
 
