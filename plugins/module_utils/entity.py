120ba18e70177d33727d672fb287e391c0b0f51a
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2024-09-30 11:43:12 +0200

    add msg if API response can't be converted (#486)
    * add msg if API response cant be converted
    
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>
    
    * add default response
    
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>
    
    * catch ValueError and increase msg length check
    
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>
    
    * revert  msg length check
    
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>
    
    * align with code standard
    
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>
    
    * fix syntax
    
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>
    
    * revert back to the beginning
    
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- plugins/module_utils/entity.py
+++ plugins/module_utils/entity.py
@@ -411,8 +411,13 @@ class Entity(object):
             return {"status_code": status_code}
 
         if resp_json is None:
+            if info.get("msg"):
+                resp_json_msg = "{}".format(info.get("msg"))
+            else:
+                resp_json_msg = "Failed to convert API response to json"
+
             self.module.fail_json(
-                msg="Failed to convert API response to json",
+                msg=resp_json_msg,
                 status_code=status_code,
                 error=body,
                 response=resp_json,
