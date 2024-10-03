fb58b522fd1d59bbe5c5f84a1cab6e7a59935643
abhinavbansal29 <abhinav.bansal1@nutanix.com>
2024-10-03 12:27:59 +0530

    Added fix for purpose field. Added description field in ansible. Issue: https://github.com/nutanix/nutanix.ansible/issues/485

--- plugins/modules/ntnx_security_rules.py
+++ plugins/modules/ntnx_security_rules.py
@@ -61,6 +61,10 @@ options:
     description: security_rule Name
     required: false
     type: str
+  description:
+    description: security_rule Description
+    required: false
+    type: str
   security_rule_uuid:
     description:
         - security_rule UUID
@@ -881,6 +885,7 @@ EXAMPLES = r"""
 - name: create app security rule
   ntnx_security_rules:
     name: test_app_rule
+    description: App Security Rule
     allow_ipv6_traffic: true
     policy_hitlog: true
     app_rule:
@@ -986,6 +991,7 @@ spec:
   type: dict
   sample:
     name: test_app_rule
+    description: App Security Rule
     resources:
       allow_ipv6_traffic: true
       app_rule:
@@ -1184,6 +1190,7 @@ def get_module_spec():
     )
     module_args = dict(
         name=dict(type="str"),
+        description=dict(type="str"),
         security_rule_uuid=dict(type="str"),
         allow_ipv6_traffic=dict(type="bool"),
         policy_hitlog=dict(type="bool"),
