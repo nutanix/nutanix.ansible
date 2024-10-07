1e75a6169cbce85c0ea5c1d4d689ed95596a24c4
george-ghawali <george.ghawali@nutanix.com>
2024-10-07 11:38:06 +0300

    Adding imprv where user can group VMs by project name

--- plugins/inventory/ntnx_prism_vm_inventory.py
+++ plugins/inventory/ntnx_prism_vm_inventory.py
@@ -184,6 +184,12 @@ class InventoryModule(BaseInventoryPlugin, Constructable):
                 self.inventory.set_variable(
                     vm_name, "ntnx_categories", entity["metadata"]["categories"]
                 )
+            if "metadata" in entity and "project_reference" in entity["metadata"]:
+                self.inventory.set_variable(
+                    vm_name,
+                    "ntnx_project",
+                    entity["metadata"]["project_reference"]["name"],
+                )
 
             # Add variables created by the user's Jinja2 expressions to the host
             self._set_composite_vars(
