3993d839282e19770491b0083aae043c9c33ec91
George Ghawali <george.ghawali@nutanix.com>
2025-01-13 16:27:43 +0200

    flake8 fix

--- plugins/modules/ntnx_promote_protected_resources_v2.py
+++ plugins/modules/ntnx_promote_protected_resources_v2.py
@@ -39,17 +39,12 @@ EXAMPLES = r"""
 RETURN = r"""
 """
 
-import traceback  # noqa: E402
-
-from ansible.module_utils.basic import missing_required_lib  # noqa: E402
-
 from ..module_utils.base_module import BaseModule  # noqa: E402
 from ..module_utils.utils import remove_param_with_none_value  # noqa: E402
 from ..module_utils.v4.data_protection.api_client import (  # noqa: E402
     get_protected_resource_api_instance,
 )
 from ..module_utils.v4.prism.tasks import wait_for_completion  # noqa: E402
-from ..module_utils.v4.spec_generator import SpecGenerator  # noqa: E402
 from ..module_utils.v4.utils import (  # noqa: E402
     raise_api_exception,
     strip_internal_attributes,
