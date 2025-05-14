c85941d50314f2803efd7b549bc9d3efabe9c46f
George Ghawali <george.ghawali@nutanix.com>
2025-05-14 15:33:34 +0300

    functional fix & sanity docs fixes

--- plugins/module_utils/v3/prism/vms.py
+++ plugins/module_utils/v3/prism/vms.py
@@ -298,7 +298,8 @@ class VM(Prism):
                 nic["mac_address"] = network["mac_address"]
 
             nic["is_connected"] = network["is_connected"]
-            nic["vlan_mode"] = network["vlan_mode"]
+            if network.get("vlan_mode"):
+                nic["vlan_mode"] = network["vlan_mode"]
             if network.get("subnet"):
 
                 if network.get("subnet", {}).get("uuid"):
