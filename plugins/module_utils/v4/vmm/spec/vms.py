0859b732ab76da5ba5063a4f0a4b6fd4028e4b78
JKindonNtx <James.Kindon@nutanix.com>
2025-05-19 04:39:54 -0700

    update per issue 579 (#639)
    * update per issue 579
    
    * update documentation modules per failed sanity tests
    
    * mirror boot_device spec for UEFI
    
    * changes in module descriptions
    
    * module description updates
    
    * test modules
    
    * updates per Georges request
    
    * move check mode configs to dummy values rather than vars
    
    * sanity tests and lint changes

--- plugins/module_utils/v4/vmm/spec/vms.py
+++ plugins/module_utils/v4/vmm/spec/vms.py
@@ -323,6 +323,15 @@ class VmSpecs:
         nvram_device=dict(
             type="dict", options=nvram_device_spec, obj=vmm_sdk.NvramDevice
         ),
+        boot_device=dict(
+            type="dict",
+            options=boot_device_spec,
+            obj=boot_device_allowed_types,
+            mutually_exclusive=[("boot_device_disk", "boot_device_nic")],
+        ),
+        boot_order=dict(
+            type="list", elements="str", choices=["CDROM", "NETWORK", "DISK"]
+        ),
     )
 
     boot_config_spec = dict(
