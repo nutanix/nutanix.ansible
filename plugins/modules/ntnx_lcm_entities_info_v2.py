250b526e90340a46be454c890c3682bd7d965b3c
George Ghawali <george.ghawali@nutanix.com>
2025-07-29 10:29:04 +0300

    Adding total available results to lcm entities

--- plugins/modules/ntnx_lcm_entities_info_v2.py
+++ plugins/modules/ntnx_lcm_entities_info_v2.py
@@ -152,6 +152,9 @@ def get_entities(module, api_instance, result):
             msg="Api Exception raised while fetching LCM entities info",
         )
 
+    total_available_results = resp.metadata.total_available_results
+    result["total_available_results"] = total_available_results
+
     resp = strip_internal_attributes(resp.to_dict()).get("data")
     if not resp:
         resp = []
