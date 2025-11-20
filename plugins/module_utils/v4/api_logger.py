04f5fece5520bfd8c1a478c9f465e32e1c0877d6
George Ghawali <george.ghawali@nutanix.com>
2025-11-20 10:42:41 +0200

    change in path

--- plugins/module_utils/v4/api_logger.py
+++ plugins/module_utils/v4/api_logger.py
@@ -194,7 +194,7 @@ class APILogger:
         """Write log message to all outputs."""
         self.module.log(log_message)
 
-        log_file = os.environ.get("NUTANIX_LOG_PATH", "/var/tmp/nutanix_ansible_debug.log")
+        log_file = os.environ.get("NUTANIX_LOG_PATH", "/var/nutanix_ansible_debug.log")
         if log_file:
             try:
                 with open(log_file, "a") as f:
