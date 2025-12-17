3390e370ed610ea994cf3900c109a9b1780e503c
Abhinav Bansal <abhinav.bansal1@nutanix.com>
2025-12-17 13:00:32 +0000

    Minor doc fix

--- plugins/modules/ntnx_lcm_config_v2.py
+++ plugins/modules/ntnx_lcm_config_v2.py
@@ -65,7 +65,7 @@ options:
     has_module_auto_upgrade_enabled:
         description:
             - Indicates if LCM is enabled to auto-upgrade products.
-            - Used when auto_inventory_enabled is set to True.
+            - Can only be set to True when auto_inventory_enabled is set to True.
         type: bool
         required: false
 extends_documentation_fragment:
