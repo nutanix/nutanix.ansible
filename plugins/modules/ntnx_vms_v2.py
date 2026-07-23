e510ce5deeb196bc9a14ceb4ae00102a0f25f0d1
Abhinav Bansal <abhinavbansal349@gmail.com>
2026-07-23 13:42:36 +0530

    Added Check mode before idempotency in some modules (#1304)

--- plugins/modules/ntnx_vms_v2.py
+++ plugins/modules/ntnx_vms_v2.py
@@ -1494,14 +1494,14 @@ def update_vm(module, result):
     ):
         update_spec.apc_config.cpu_model = None
 
+    if module.check_mode:
+        result["response"] = strip_internal_attributes(update_spec.to_dict())
+        return
+
     # check for idempotency
     if check_idempotency(current_spec, update_spec):
         result["skipped"] = True
         module.exit_json(msg="Nothing to change.", **result)
-
-    if module.check_mode:
-        result["response"] = strip_internal_attributes(update_spec.to_dict())
-        return
 
     resp = None
     try:
