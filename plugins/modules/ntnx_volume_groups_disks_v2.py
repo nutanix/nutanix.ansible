e030d34ef2331b5757b9993d6d89d6cf5e107da7
George Ghawali <george.ghawali@nutanix.com>
2026-09-02 09:51:00 +0300

    isort fix

--- plugins/modules/ntnx_volume_groups_disks_v2.py
+++ plugins/modules/ntnx_volume_groups_disks_v2.py
@@ -258,9 +258,7 @@ from ..module_utils.v4.volumes.api_client import (  # noqa: E402
     get_etag,
     get_vg_api_instance,
 )
-from ..module_utils.v4.volumes.spec.volume_group import (  # noqa: E402
-    VGSpecs as vg_specs,
-)
+from ..module_utils.v4.volumes.spec.volume_group import VGSpecs as vg_specs  # noqa: E402
 
 SDK_IMP_ERROR = None
 try:
