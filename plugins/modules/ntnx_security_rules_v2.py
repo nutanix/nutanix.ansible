7bcfa1003b9273e01db2348342e280cbfa9c5f20
George Ghawali <george.ghawali@nutanix.com>
2026-09-07 12:38:14 +0300

    remove debug statements

--- plugins/modules/ntnx_security_rules_v2.py
+++ plugins/modules/ntnx_security_rules_v2.py
@@ -1238,8 +1238,6 @@ def update_network_security_policy(module, result):
         update_spec.state = current_spec.state
 
     # check for idempotency
-    result["current_spec_dict"] = current_spec.to_dict()
-    result["update_spec_dict"] = update_spec.to_dict()
     current_spec_dict = strip_internal_attributes(current_spec.to_dict())
     update_spec_dict = strip_internal_attributes(update_spec.to_dict())
     if check_network_security_policies_idempotency(current_spec_dict, update_spec_dict):
